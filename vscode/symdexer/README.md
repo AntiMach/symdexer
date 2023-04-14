8505e24bbc6be40ecd6095639c21de9ad3f1c389
AntiMach <themachinumps@gmail.com>
2023-04-14 03:15:47 +0100

    extension update

--- vscode/symdexer/README.md
+++ vscode/symdexer/README.md
@@ -1,71 +1,49 @@
-# symdexer README
+# Symdexer (Python)
 
-This is the README for your extension "symdexer". After writing up a brief description, we recommend including the following sections.
+A python utilitly for quickly indexing symbols over specific, hard to index, directory trees.
+
+Written in Python (CLI) and TypeScript (VSC Extension).
+
+**[Repository](https://github.com/antimach/symdexer)**
+
+**[PyPi](https://pypi.org/project/symdexer/)**
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Initialize symbol indexing configuration on your current workspace.
 
-For example if there is an image subfolder under your extension project workspace:
+Create/Reset the index cache for symbol lookup.
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Instantly find the best import for the word the cursor is over.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+To run this extension, you must have at least `Python 3.11` installed and the
+`symdexer` pip package installed.
+
+```sh
+# install symdexer
+pip install symdexer
+# check if symdexer was installed successully
+symdexer -h
+```
+
+`symdexer` must be accessible via the environment's PATH.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+This extension provides the following configuration:
 
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+* `symdexer.configFile`: Specifies a file to use at the root of a workspace
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Very specific at what it does still.
+
+*And quite possibly more than that*.
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release
--- vscode/symdexer/src/extension.ts
+++ vscode/symdexer/src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { spawn } from 'child_process';
 
-import {spawn} from 'child_process';
 
-
-function runInit() {
+function runCommand(cwd: string, ...args: string[]) {
 	return new Promise<string>((resolve, reject) => {
-		const proc = spawn("symdexer", ["init"]);
+		const proc = spawn(args[0], args.slice(1), {
+			cwd: cwd
+		});
 
 		let result = "";
 		let error = "";
@@ -33,21 +35,124 @@ function runInit() {
 	});
 }
 
+
+function showBusy<T>(title: string, promise: Promise<T>) {
+	return new Promise<T>((resolve, reject) => {
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Window,
+			cancellable: false,
+			title: title,
+		}, async (prog) => {
+			prog.report({increment: 0});
+
+			let result: any, cb;
+
+			try {
+				result = await promise;
+				cb = resolve;
+			}
+			catch (err) {
+				result = err;
+				cb = reject;
+			}
+
+			prog.report({increment: 100});
+
+			cb(result);
+		});
+	});
+}
+
+
+async function getWorkspace(): Promise<string> {
+	if (vscode.workspace.workspaceFolders?.length === 1) {
+		return vscode.workspace.workspaceFolders[0].uri.fsPath;
+	}
+
+	let f = await vscode.window.showWorkspaceFolderPick();
+	return f!.uri.fsPath;
+}
+
+
+function getConfigFile(): string {
+	return vscode.workspace.getConfiguration("symdexer")!.get("configFile")!;
+}
+
+async function runInit(): Promise<string> {
+	let cwd = await getWorkspace();
+	let conf = getConfigFile();
+
+	let result = await runCommand(cwd, 'symdexer', '-s', conf, 'init');
+	
+	vscode.window.showTextDocument(
+		await vscode.workspace.openTextDocument(
+			path.join(cwd, conf)
+		)
+	);
+
+	return result;
+}
+
+async function runCache(): Promise<string> {
+	let cwd = await getWorkspace();
+	let conf = getConfigFile();
+	
+	return await showBusy(
+		'Indexing workspace',
+		runCommand(cwd, 'symdexer', '-s', conf, 'reset-cache')
+	);
+}
+
+async function runFind(editor: vscode.TextEditor): Promise<string> {
+	let cwd = await getWorkspace();
+	let conf = getConfigFile();
+
+	let pos = editor.selection.anchor;
+	let range = editor.document.getWordRangeAtPosition(pos)!;
+	let word = editor.document.getText(range);
+
+	let result = await runCommand(cwd, 'symdexer', '-s', conf, 'find', word);
+	let items = result.split("\n").filter(v => v !== "");
+
+	if (items.length === 0) {
+		return 'no items found';
+	}
+
+	let selection = await vscode.window.showQuickPick(items, {
+		title: 'Results'
+	}) ?? '';
+
+	if (selection === '') {
+		return '';
+	}
+
+	await editor.edit(eb => {
+		eb.insert(new vscode.Position(0, 0), selection + '\n');
+	});
+
+	return '';
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('symdexer.init', () => {
-			runInit().then(v => {
-				vscode.window.showInformationMessage(v);
-			}, e => {
-				vscode.window.showErrorMessage(e);
-			});
+			runInit().then(
+				vscode.window.showInformationMessage,
+				vscode.window.showErrorMessage,
+			);
 		}),
 		vscode.commands.registerCommand('symdexer.cache', () => {
-			vscode.window.showInformationMessage('Hello World from symdexer!');
+			runCache().then(
+				vscode.window.showInformationMessage,
+				vscode.window.showErrorMessage
+			);
 		}),
-		vscode.commands.registerCommand('symdexer.find', () => {
-			vscode.window.showInformationMessage('Hello World from symdexer!');
-		})
+		vscode.commands.registerTextEditorCommand('symdexer.find', (editor, edit) => {
+			runFind(editor).then(
+				vscode.window.showInformationMessage,
+				vscode.window.showErrorMessage
+			);
+		}),
 	);
 }
 
